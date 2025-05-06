c0ce58404943f45f563b7dc7676321486e37a8c5
Helvin Tañada Jr <120767948+Helvin02@users.noreply.github.com>
2025-05-06 22:40:57 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-// Replace the content of App.js with this:
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LungEvityUI from './LungEvityUI';
-import Login from './Login';
-import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LungEvityUI />} />
-        <Route path="/login" element={<Login />} />
-        {/* Add more routes as needed */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <LungEvityUI />
+    </div>
   );
 }
 
